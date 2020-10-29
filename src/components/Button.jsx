28df855b8f39cbed67789a1d56d9461120a33fb2
joieloliveira <59665976+joieloliveira@users.noreply.github.com>
2020-10-28 21:23:22 -0300

    Add files via upload
    Primeira metade da calculadora escrita em react

---
+++ src/components/Button.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Button.css'
+
+export default props => {
+
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
+
+
+/*
+//dentro de `` sera usado js puro
+<button className={`
+    button
+    ${props.operation ? 'operation' : ''}
+    ${props.double ? 'double' : ''}
+    ${props.triple ? 'triple' : ''}
+`}>
+    {props.label}
+</button>
+*/
